f1ee5cfb34dbd9edc80688b2af12a865ef0eeb96
Jhashua10 <146078820+Jhashua10@users.noreply.github.com>
2025-06-25 17:15:35 -0600

    Update database.js

--- API/src/config/database.js
+++ API/src/config/database.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-console.log('🟡 Verificando variables de entorno:');
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_HOST:', process.env.DB_HOST);
+
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
