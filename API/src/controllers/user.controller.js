62f570c6876dccdbc7e597c44c4061607c864f48
Jhashua10 <146078820+Jhashua10@users.noreply.github.com>
2025-06-25 17:09:06 -0600

    changes

--- API/src/controllers/user.controller.js
+++ API/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model');
 const Role = require('../models/roles.model');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const getAllUsers = async(req, res) => {
     try {
