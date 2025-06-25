fa74e058ecca91f1ce28b4efa263d35bd3b7b6f1
Jhashua10 <146078820+Jhashua10@users.noreply.github.com>
2025-06-25 16:49:38 -0600

    changes

--- API/src/config/database.js
+++ API/src/config/database.js
@@ -1,23 +1,28 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+console.log('🟡 Verificando variables de entorno:');
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+
 const sequelize = new Sequelize(
-     
-  "p4_project",     //nombre de la base de datos
-  "root", //usuario
-  "1234", //contraseña 
-  {
-    host: process.env.DB_HOST, 
-    dialect: 'mysql',         
-    logging: false,           
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+    }
 );
 
 sequelize.authenticate()
-  .then(() => {
-    console.log('Conexión a la base de datos establecida con éxito.');
-  })
-  .catch(err => {
-    console.error('No se pudo conectar a la base de datos:', err);
-  });
+    .then(() => {
+        console.log('Conexión a la base de datos establecida con éxito.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos:', err);
+    });
 
 module.exports = sequelize;
