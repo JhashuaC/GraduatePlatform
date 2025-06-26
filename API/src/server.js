a3617dfaef5e77e776565ac228a3b53ef35faec6
Jhashua10 <146078820+Jhashua10@users.noreply.github.com>
2025-06-25 18:54:37 -0600

    Update server.js

--- API/src/server.js
+++ API/src/server.js
@@ -23,7 +23,15 @@ const surveyResponsesRoutes = require('./routes/survey_responses.routes');
 const notesRoutes = require('./routes/notes.routes');
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://jhasmartech.online"
+    ],
+    credentials: true
+}));
+
 app.use(morgan('dev'));
 app.use(express.json());
 
