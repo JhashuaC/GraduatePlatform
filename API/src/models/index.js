3fdceafa88a8e51a3f4f3e9aa7569c0a670284f9
Jhashua10 <jhashua10@gmail.com>
2025-06-26 17:59:44 -0600

    Remove redundant comments and whitespace in model associations
    Cleaned up the index.js file by removing unnecessary comments and extra blank lines between model association definitions. This improves readability and maintains consistency in the codebase.

--- API/src/models/index.js
+++ API/src/models/index.js
@@ -14,30 +14,25 @@ const EmailRecipient = require('./email_recipients.model');
 const SurveyQuestion = require('./survey_questions.model');
 const SurveyResponse = require('./survey_responses.model');
 
-// User & Role
 User.belongsTo(Role, { foreignKey: 'id_role' });
 Role.hasMany(User, { foreignKey: 'id_role' });
 
-// User & Graduate / Speaker
 Graduate.belongsTo(User, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 User.hasOne(Graduate, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 
 Speaker.belongsTo(User, { foreignKey: 'id_speaker', onDelete: 'CASCADE' });
 User.hasOne(Speaker, { foreignKey: 'id_speaker', onDelete: 'CASCADE' });
 
-// Graduate & Career
 Graduate.belongsTo(Career, { foreignKey: 'id_career' });
 Career.hasMany(Graduate, { foreignKey: 'id_career' });
 
-// Course & Speaker
 Course.belongsTo(Speaker, { foreignKey: 'id_speaker' });
 Speaker.hasMany(Course, { foreignKey: 'id_speaker' });
 
-// CourseGraduate
 CourseGraduate.belongsTo(Course, { foreignKey: 'id_course', onDelete: 'CASCADE' });
 CourseGraduate.belongsTo(Graduate, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 
-// CareerCourse (M:N)
+
 Career.belongsToMany(Course, {
     through: CareerCourse,
     foreignKey: 'id_career',
@@ -47,7 +42,7 @@ Course.belongsToMany(Career, {
     foreignKey: 'id_course',
 });
 
-// CourseGraduate (M:N)
+
 Course.belongsToMany(Graduate, {
     through: CourseGraduate,
     foreignKey: 'id_course',
@@ -57,7 +52,7 @@ Graduate.belongsToMany(Course, {
     foreignKey: 'id_graduate',
 });
 
-// CourseCategory (M:N)
+
 Course.belongsToMany(PreferenceOption, {
     through: CourseCategory,
     foreignKey: 'id_course',
@@ -67,7 +62,7 @@ PreferenceOption.belongsToMany(Course, {
     foreignKey: 'id_option',
 });
 
-// GraduatePreference (M:N)
+
 GraduatePreference.belongsTo(Graduate, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 GraduatePreference.belongsTo(PreferenceOption, { foreignKey: 'id_option', onDelete: 'CASCADE' });
 
@@ -80,11 +75,11 @@ PreferenceOption.belongsToMany(Graduate, {
     foreignKey: 'id_option',
 });
 
-// EmailHistory & User
+
 EmailHistory.belongsTo(User, { foreignKey: 'id_admin', onDelete: 'CASCADE' });
 User.hasMany(EmailHistory, { foreignKey: 'id_admin' });
 
-// EmailRecipients (M:N)
+
 EmailHistory.belongsToMany(Graduate, {
     through: EmailRecipient,
     foreignKey: 'id_email',
@@ -97,7 +92,7 @@ Graduate.belongsToMany(EmailHistory, {
 EmailRecipient.belongsTo(EmailHistory, { foreignKey: 'id_email', onDelete: 'CASCADE' });
 EmailRecipient.belongsTo(Graduate, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 
-// SurveyResponse
+
 SurveyResponse.belongsTo(Graduate, { foreignKey: 'id_graduate', onDelete: 'CASCADE' });
 Graduate.hasMany(SurveyResponse, { foreignKey: 'id_graduate' });
 
@@ -107,7 +102,7 @@ Course.hasMany(SurveyResponse, { foreignKey: 'id_course' });
 SurveyResponse.belongsTo(SurveyQuestion, { foreignKey: 'id_question', onDelete: 'CASCADE' });
 SurveyQuestion.hasMany(SurveyResponse, { foreignKey: 'id_question' });
 
-// Export all
+
 module.exports = {
     User,
     Role,
